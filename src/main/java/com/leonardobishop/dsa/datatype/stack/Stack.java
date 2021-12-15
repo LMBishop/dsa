fdd5fca076639f244748fd28b458f54cc12486c1
LMBishop <13875753+LMBishop@users.noreply.github.com>
2021-12-15 12:15:53 +0000

    Add javadoc to stack interface

--- src/main/java/com/leonardobishop/dsa/datatype/stack/Stack.java
+++ src/main/java/com/leonardobishop/dsa/datatype/stack/Stack.java
@@ -2,10 +2,22 @@ package com.leonardobishop.dsa.datatype.stack;
 
 public interface Stack<E> {
 
+    /**
+     * Inserts an element onto the top of stack.
+     * @param element element to push
+     */
     void push(E element);
 
+    /**
+     * Removes the top element off the stack and returns it.
+     * @return the popped element
+     */
     E pop();
 
+    /**
+     * Returns the size of the stack.
+     * @return size of stack
+     */
     int size();
 
 }
